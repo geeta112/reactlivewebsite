d90f80e3db133280b7bb1483fa95158a07bfa9d0
geeta112 <geetarajput66689@gmail.com>
2022-01-01 15:48:53 +0530

    commit

---
+++ build/precache-manifest.300138acc3cba2c91aa1efc10f55702b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1c273fde0a2230aeefe",
+    "url": "/reactlivewebsite/static/css/main.456b5daa.chunk.css"
+  },
+  {
+    "revision": "e1c273fde0a2230aeefe",
+    "url": "/reactlivewebsite/static/js/main.e1c273fd.chunk.js"
+  },
+  {
+    "revision": "90170fff6c8c6a5248df",
+    "url": "/reactlivewebsite/static/css/1.e1601801.chunk.css"
+  },
+  {
+    "revision": "90170fff6c8c6a5248df",
+    "url": "/reactlivewebsite/static/js/1.90170fff.chunk.js"
+  },
+  {
+    "revision": "18a195a03cc8ea014908",
+    "url": "/reactlivewebsite/static/js/runtime~main.18a195a0.js"
+  },
+  {
+    "revision": "759253f7891d18c0dc7f01d171e606b9",
+    "url": "/reactlivewebsite/static/media/log-in-girl.759253f7.svg"
+  },
+  {
+    "revision": "571b2807ca409f2ff264fefc317ff903",
+    "url": "/reactlivewebsite/static/media/blogging.571b2807.svg"
+  },
+  {
+    "revision": "9359e6a29ed4a4ff3c2f17102e6eba16",
+    "url": "/reactlivewebsite/static/media/S3.9359e6a2.jpg"
+  },
+  {
+    "revision": "3c860608220b355f5e838741613f94af",
+    "url": "/reactlivewebsite/static/media/S5.3c860608.jpg"
+  },
+  {
+    "revision": "d15a084517ef36ada55bc286692134a7",
+    "url": "/reactlivewebsite/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.76562325cb2f884e5ce68f056b0c1b89.js"
+  "/reactlivewebsite/precache-manifest.300138acc3cba2c91aa1efc10f55702b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/reactlivewebsite/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
