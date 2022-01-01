5330fbd47993d5b4b055f3eb6373b28251bd5254
webbywolf <info.webbywolf@gmail.com>
2022-01-01 12:45:01 +0530

    commit

---
+++ build/precache-manifest.d30db7abb81bfc2cb29fc4102fcc9c84.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8688942b8903f42eccc5",
+    "url": "/reactlivewebsite/static/css/main.4c1d875c.chunk.css"
+  },
+  {
+    "revision": "8688942b8903f42eccc5",
+    "url": "/reactlivewebsite/static/js/main.8688942b.chunk.js"
+  },
+  {
+    "revision": "8de79aff9b5b5b37c006",
+    "url": "/reactlivewebsite/static/css/1.3200224f.chunk.css"
+  },
+  {
+    "revision": "8de79aff9b5b5b37c006",
+    "url": "/reactlivewebsite/static/js/1.8de79aff.chunk.js"
+  },
+  {
+    "revision": "18a195a03cc8ea014908",
+    "url": "/reactlivewebsite/static/js/runtime~main.18a195a0.js"
+  },
+  {
+    "revision": "759253f7891d18c0dc7f01d171e606b9",
+    "url": "/reactlivewebsite/static/media/log-in-girl.759253f7.svg"
+  },
+  {
+    "revision": "571b2807ca409f2ff264fefc317ff903",
+    "url": "/reactlivewebsite/static/media/blogging.571b2807.svg"
+  },
+  {
+    "revision": "9359e6a29ed4a4ff3c2f17102e6eba16",
+    "url": "/reactlivewebsite/static/media/S3.9359e6a2.jpg"
+  },
+  {
+    "revision": "3c860608220b355f5e838741613f94af",
+    "url": "/reactlivewebsite/static/media/S5.3c860608.jpg"
+  },
+  {
+    "revision": "d947208a9fa458d1bc660d5541ab3dcc",
+    "url": "/reactlivewebsite/index.html"
+  }
+];
